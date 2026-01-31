2d242fc3991266c5d5e2cb851e5e68e2a7725dc8
LUQMAN <luqman5421er@gmail.com>
2026-01-31 16:00:37 +0800

    Add files via upload

---
+++ MILESTONE2MAIN.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+void
+
+
+
+int main()
+{
+    cout << "=============================================\n";
+    cout << "STUDENT ATTENDANCE TRACKER - MILESTONE 1\n";
+    cout << "=============================================\n\n";
+}

c65530636370f09887f728f4e0c852afbb78bfe0
Dairell <dairellhannan@yahoo.com>
2026-01-31 16:34:14 +0800

    try test

--- MILESTONE2MAIN.cpp
+++ MILESTONE2MAIN.cpp
@@ -4,7 +4,42 @@
 using namespace std;
 
 
-void
+
+    int validate_student_id (string prompt)
+    {
+        int id;
+        while (true) 
+        {
+            cout << prompt;
+            if (cin >> id)
+            {
+                cin.ignore();
+                return id;
+            }
+            else
+            {
+                cout << "ERROR: Invalid integer. Try again." << endl;
+                cin.clear();
+                cin.ignore();
+            }
+            }
+    }
+
+    int validate_status(string prompt)
+    {
+        int status;
+        while (true)
+        {
+            status = validate_student_id(prompt);
+            if (status == 0 || status == 1) return status;
+            cout << "ERROR: Status must be 0 or 1" << endl;
+        }
+    }
+void report_missing_id (int id)
+{
+    cout << "ERROR: Student ID does not exist." << endl;
+}
+
 
 
 
@@ -13,4 +48,10 @@ int main()
     cout << "=============================================\n";
     cout << "STUDENT ATTENDANCE TRACKER - MILESTONE 1\n";
     cout << "=============================================\n\n";
+
+    cout << "---Advanced Error Handling Demo---" << endl;
+
+    int idInput = validate_student_id("Enter StudentID to update: ");
+    int idStatus = validate_status("Enter status: ");
+
 }
